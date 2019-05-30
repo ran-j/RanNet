eee3742b6628790985bbfdd5af599e5366c5a75d
root <root@vps9303.publiccloud.com.br>
2019-05-30 14:44:19 -0300

    Add clusters

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,8 @@ module.exports = {
           name: "RanNet",
           script: "./bin/www",
           watch: false,
+	  instances : "max",
+	  exec_mode : "cluster", 
           env: {
               "PORT": 3001,
               "NODE_ENV": "production"
