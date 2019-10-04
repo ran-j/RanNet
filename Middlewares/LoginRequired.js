cf5a749c9165433d711bd10b06dcaa5e0f926aa9
Ranieri Abreu Silva Junior <rabreu@indracompany.com>
2019-10-04 11:00:06 -0300

    fix login page and midleware

--- Middlewares/LoginRequired.js
+++ Middlewares/LoginRequired.js
@@ -6,7 +6,7 @@ const requiresLogin = (req, res, next) => {
     res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     return next();
   } else {
-    next(createError(404)); 
+    next(createError(403)); 
   }
 
 }
